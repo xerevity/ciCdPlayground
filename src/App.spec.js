ae5ad5acda2ccb3b97c7485f49b7f18f0c6ca42b
Christof Happ <christof.happ@tngtech.com>
2022-01-21 16:27:15 +0100

    add a test (and a few testing dependencies)

---
+++ src/App.spec.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+// NOTE: jest-dom adds handy assertions to Jest and it is recommended, but not required.
+import '@testing-library/jest-dom'
+
+import {render, fireEvent} from '@testing-library/svelte'
+
+import App from './App.svelte'
+
+test('shows proper heading when rendered', () => {
+  const {getByText} = render(App, {name: 'World'})
+
+  expect(getByText('Hello World!')).toBeInTheDocument()
+})
