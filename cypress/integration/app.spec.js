ebd0dd8f1a6cd72e628b9bc04b29413a3fde84f4
Christof Happ <christof.happ@tngtech.com>
2022-12-02 17:54:45 +0100

    * make e2e tests run with the new port * use config instead of hardcoding e2e test base url

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,7 +2,7 @@
 //
 describe('The app', () => {
     it('tells that you are a butler', () => {
-        cy.visit('http://localhost:8080/')
+        cy.visit('')
         cy.get('h1')
         .should('have.text', 'Hello, my name is James Butler!')
     })
