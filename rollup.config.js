d41e9e4f9a4f6782c953015c986010ef60851d24
Christof Happ <christof.happ@tngtech.com>
2023-12-11 13:32:25 +0100

    Upgrade to Rollup 3 and adapt config accordingly

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import { spawn } from 'child_process';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -17,7 +18,7 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+			server = spawn('npm', ['run', 'start', '--', '--dev'], {
 				stdio: ['ignore', 'inherit', 'inherit'],
 				shell: true
 			});
@@ -72,5 +73,6 @@ export default {
 	],
 	watch: {
 		clearScreen: false
-	}
+	},
+	strictDeprecations: true
 };
